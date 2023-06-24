cb3ec4aaa3156cb40b432f5b55f44842af784dbc
SemionStepan <95352195+SemionStepan@users.noreply.github.com>
2023-06-24 08:34:13 +0300

    Add files via upload

--- trainBoard/main.cpp
+++ trainBoard/main.cpp
@@ -2,6 +2,18 @@
 
 int main()
 {
-    cout << "Hello World!" << endl;
+
+vector <Train> vec
+{
+    Train("Volgograd", 1, 12, 10),
+    Train("Moskva", 3, 13, 15),
+    Train("St Petersburg", 2, 14, 30),
+    Train("Vladivostok", 4, 15, 48),
+    Train("Volgograd", 5, 18, 59)
+};
+
+Trains trains(vec);
+Trains sortTrains();
+trains.out();
     return 0;
 }
--- trainBoard/trainBoard.cpp
+++ trainBoard/trainBoard.cpp
@@ -2,6 +2,67 @@
 
 void Train::out()
 {
-    std::cout << "[" << _destinationName << "] {" << _trainNumber << "} " << endl;
-//<< _departureTime
+    std::cout << "[" << _destinationName << "] {" << _trainNumber << "} " << _departureHour << ':' << _departureMinute << endl;
 }
+
+int Train::getTrainNumber()
+{
+    return _trainNumber;
+}
+
+int Train::getDepartureHour()
+{
+    return _departureHour;
+}
+
+int Train::getDepartureMinute()
+{
+    return _departureMinute;
+}
+
+void Trains::out()
+{
+    for(auto train : _trains)
+    {
+        train.out();
+    }
+}
+
+void Trains::sortTrains()
+{
+    sort(_trains.begin(), _trains.end(),
+        [](Train &a, Train &b)
+        {
+            if(b.getTrainNumber() == a.getTrainNumber())
+            {
+                if(b.getDepartureMinute() > a.getDepartureMinute())
+                {
+                    return b.getDepartureMinute() > a.getDepartureMinute();
+                }
+                return b.getDepartureHour() > a.getDepartureHour();
+            }
+            return b.getTrainNumber() > a.getTrainNumber();
+         }
+         );
+}
+
+void Trains::selectedOut()
+{
+/*    switch () Здесь свитч, который выбирает ввод с клавиатуры, cin
+    {
+        case 1: первый поезд
+            _trains. первыйПоезд.Вывод
+            break;
+        case 2:
+        break;
+        case 3:
+        break;
+        case 4:
+        break;
+        case 5:
+        break;
+        default:
+            std::cout << "Вы выбрали неверный номер" << endl;
+            break;
+}
+*/    }
